3a6ea52bf3bb0dfa0bfe8da03917563db0e02ca0
Ryan Carey <ryaanski18@yahoo.com>
2022-02-15 18:09:35 -0500

    Updates

---
+++ precache-manifest.4b13617e82c2733dcb3359b75a63b860.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231a4ba7742d7503cebb804cd5146751",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "287013eccc1e2bbb786d",
+    "url": "/react-portfolio/static/css/main.08325502.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/react-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287013eccc1e2bbb786d",
+    "url": "/react-portfolio/static/js/main.2b04411b.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/react-portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/react-portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/react-portfolio/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/react-portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/react-portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/react-portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
