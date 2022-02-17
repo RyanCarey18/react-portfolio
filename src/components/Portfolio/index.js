2a7eff753ca736ef50b40980edebeaa7c3f7ec50
Ryan Carey <ryaanski18@yahoo.com>
2022-02-16 22:21:53 -0500

    added all projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,39 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Project from "../Project";
 
 function Portfolio() {
-
   // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: 'surf-report',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: "grocery-application",
+      description:
+        "HTML, CSS, Bootstrap, Handlebars, SQL, Sequelize, Express.js,Node.js",
+      link: "https://vast-ocean-43647.herokuapp.com/",
+      repo: "https://github.com/RyanCarey18/grocery-application",
     },
     {
-      name: 'pastel-puzzels',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: "marvel-search",
+      description: "HTML, JavaScript, CSS, API, JQuery, GoogleFonts",
+      link: "https://ryancarey18.github.io/Marvel-Character-Search/",
+      repo: "https://github.com/RyanCarey18/Marvel-Character-Search",
     },
     {
-      name: 'run-buddy',
-      description: 'HTML/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: "techblog",
+      description:
+        "HTML, CSS, Bootstrap, Handlebars, SQL, Sequelize, Express.js, Node.js",
+      link: "https://murmuring-shelf-05135.herokuapp.com/",
+      repo: "https://github.com/RyanCarey18/tech-blog",
     },
     {
-      name: 'led-wall',
-      description: 'Node/IoT',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: "weather-application",
+      description: "HTML,CSS,BOOTSTRAP,JS",
+      link: "https://ryancarey18.github.io/Weather-Application/",
+      repo: "https://github.com/RyanCarey18/Weather-Application",
     },
     {
-      name: 'calculator',
-      description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: "workday-scheduler",
+      description: "PWA, Heroku, IndexDb",
+      link: "https://ryancarey18.github.io/workday-scheduler/",
+      repo: "https://github.com/RyanCarey18/workday-scheduler",
+    },
+    {
+      name: "text-editor",
+      description: "HTML, CSS, JS",
+      link: "https://ryans-text-editor.herokuapp.com/",
+      repo: "https://github.com/RyanCarey18/text-editor-pwa",
     },
   ]);
 
@@ -41,14 +48,11 @@ function Portfolio() {
     <div>
       <div className="flex-row">
         {projects.map((project, idx) => (
-          <Project
-            project={project}
-            key={"project" + idx}
-          />
+          <Project project={project} key={"project" + idx} />
         ))}
       </div>
     </div>
   );
-};
+}
 
 export default Portfolio;
