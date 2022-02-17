cb906264083fb59ea099cc986ffdaa5d352f7239
Ryan Carey <ryaanski18@yahoo.com>
2022-02-16 21:14:27 -0500

    added picture of myself. added about me. added links to github, linkedin, stackoverflow. added resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function About() {
   return (
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+          <img
+            src={require(`../../assets/me/myPic.jpg`)}
+            className="me-pic"
+            alt="Ryan"
+          />
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
+          My name is Ryan Carey and I originally went to school for accounting.
+          After deciding that I wanted more out of my work life I switched gears
+          and took a coding bootcamp through the University of New Hampshire. My
+          love of computers traces back to when I was a child and I would always
+          be going through all the settings on the family computer and wowing my
+          family with the things I was doing. I hope to acquire a job and be
+          able to turn my passion into a career.
         </p>
         <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+          Full-Stack Web Developer attempting to build a more intuitive user
+          experience on the web utilizing experience in customer service.
+          Recently earned a certificate in Full Stack Web Development from the
+          University of New Hampshire, developed skills in JavaScript, CSS,
+          React.js, Node.js, and responsive web design. Strengths in meeting
+          deadlines and teamwork working on projects to create a smooth user
+          experience that works efficiently. Worked on a team of three to
+          develop an app that helps users pick groceries at a store that
+          organizes them by aisle for a faster shopping experience—excited to
+          leverage expertise and knowledge on a fast-paced, quality-driven team
+          to maximize experiences on the web.
         </p>
       </div>
     </section>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+import React from "react";
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 function Header(props) {
-
   return (
     <header className="flex-row space-between px-1">
-      <h1>Lernantino</h1>
+      <h1>Ryan Carey</h1>
       <img src={coverImage} alt="wooden background"></img>
       {props.children}
     </header>
