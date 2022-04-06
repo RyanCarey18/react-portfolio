2a22f2556cd75537eda83c71f36eb9bef900a0dd
Ryan Carey <ryaanski18@yahoo.com>
2022-04-06 05:14:07 -0400

    Updates

---
+++ precache-manifest.c0bee05be5e0cb9bc5034adbc6774311.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d355bc7489dcb488d984e63585b055b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b31d90d8b10d9c6257d6",
+    "url": "/react-portfolio/static/css/main.4030c94d.chunk.css"
+  },
+  {
+    "revision": "6078a1a89dc463e0e918",
+    "url": "/react-portfolio/static/js/2.e3890e8f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.e3890e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31d90d8b10d9c6257d6",
+    "url": "/react-portfolio/static/js/main.45f7c1a8.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "b2499f9c4285ddbb3edc9e35fdabda8d",
+    "url": "/react-portfolio/static/media/api-social-network.b2499f9c.png"
+  },
+  {
+    "revision": "7f6a69a0917dd459666921310f0d01b7",
+    "url": "/react-portfolio/static/media/cover-image.7f6a69a0.jpg"
+  },
+  {
+    "revision": "9b97f7db00f064d48bb74c804d2d33a0",
+    "url": "/react-portfolio/static/media/grocery-application.9b97f7db.png"
+  },
+  {
+    "revision": "f864910a1be47b50c3b225aa201d8ed3",
+    "url": "/react-portfolio/static/media/marvel-search.f864910a.png"
+  },
+  {
+    "revision": "b0833db331a35724ef5e48a21c027e2a",
+    "url": "/react-portfolio/static/media/myPic.b0833db3.jpg"
+  },
+  {
+    "revision": "b58840c742af28fbe0bb990a1c0462b9",
+    "url": "/react-portfolio/static/media/sigma-cleaning.b58840c7.png"
+  },
+  {
+    "revision": "116800bd88b99666b16cf8e34de1284e",
+    "url": "/react-portfolio/static/media/team-profile-generator.116800bd.png"
+  },
+  {
+    "revision": "47e2d9cea311d03a7022e810ba20cab5",
+    "url": "/react-portfolio/static/media/techblog.47e2d9ce.png"
+  }
+]);
